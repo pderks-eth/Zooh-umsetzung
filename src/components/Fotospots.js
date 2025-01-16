0cd93c913ec66611539ee3fe85d1748ee148c418
mfedorchenko <mfedorchenko@ethz.ch>
2025-01-16 15:58:45 +0100

    Adjusted Design by Fotospots

--- src/components/Fotospots.js
+++ src/components/Fotospots.js
@@ -24,24 +24,26 @@ function Fotospots() {
             </h2>
           </li>
           <ul className={`general-notes ${isGeneralNotesVisible ? 'visible' : ''}`}>
-            <li>Erkundigen Sie sich nach den Fütterungszeiten; hier ergeben sich spannende Fotomotive.</li>
-            <li>Fotografieren Sie zu jedem Tier die Infotafel an der Anlage, damit Sie zuhause wissen, was Sie aufgenommen haben.</li>
-            <li>Legen Sie die Schärfe auf das Auge des Tieres.</li>
-            <li>Platzieren Sie das Hauptsujet nicht in die Mitte. Ihre Bilder wirken dadurch spannender.</li>
-            <li>Achten Sie auf einen geraden Horizont. Dies ist vor allem wichtig bei Aufnahmen mit Wasser (z.B. Pinguine).</li>
-            <li>Machen Sie weniger, dafür gute Bilder.</li>
-            <li>Nehmen Sie sich Zeit und warten Sie auf den besten Moment.</li>
-            <li>Stellen Sie den Weissabgleich auf «automatisch» ein.</li>
-            <li>Achten Sie darauf, folgende Verschlusszeiten nicht zu unterschreiten (Verwackelungsgefahr):  
-              Weitwinkelobjektiv 1/30s, Normalobjektiv 1/125s, Teleobjektiv 1/250s.
-            </li>
+              <div className="fotospots-image-container">
+                <img src={SnakeImage} alt="Snake" className="fotospots-image" />
+              </div>
+              <div className="fotospots-text">
+                <li>Erkundigen Sie sich nach den Fütterungszeiten; hier ergeben sich spannende Fotomotive.</li>
+                <li>Fotografieren Sie zu jedem Tier die Infotafel an der Anlage, damit Sie zuhause wissen, was Sie aufgenommen haben.</li>
+                <li>Legen Sie die Schärfe auf das Auge des Tieres.</li>
+                <li>Platzieren Sie das Hauptsujet nicht in die Mitte. Ihre Bilder wirken dadurch spannender.</li>
+                <li>Achten Sie auf einen geraden Horizont. Dies ist vor allem wichtig bei Aufnahmen mit Wasser (z.B. Pinguine).</li>
+                <li>Machen Sie weniger, dafür gute Bilder.</li>
+                <li>Nehmen Sie sich Zeit und warten Sie auf den besten Moment.</li>
+                <li>Stellen Sie den Weissabgleich auf «automatisch» ein.</li>
+                <li>
+                  Achten Sie darauf, folgende Verschlusszeiten nicht zu unterschreiten (Verwackelungsgefahr):
+                  Weitwinkelobjektiv 1/30s, Normalobjektiv 1/125s, Teleobjektiv 1/250s.
+                </li>
+              </div>
           </ul>
 
-          <li>
-            <h2 onClick={() => setIsBacklightVisible((prev) => !prev)} className="clickable-heading">
-              Bei Gegenlicht
-            </h2>
-          </li>
+          <li><h2 onClick={() => setIsBacklightVisible((prev) => !prev)} className="clickable-heading">Bei Gegenlicht</h2></li>
           <ul className={`general-notes ${isBacklightVisible ? 'visible' : ''}`}>
             <li>Vermeiden Sie starke Schatten, indem Sie bei Gegenlicht Aufhellblitze oder Reflektoren verwenden.</li>
             <li>Nutzen Sie den HDR-Modus der Kamera, um Details in hellen und dunklen Bereichen zu erhalten.</li>
@@ -126,9 +128,7 @@ function Fotospots() {
           </ul>
         </div>
 
-        <div className="fotospots-image-container">
-          <img src={SnakeImage} alt="Snake" className="fotospots-image" />
-        </div>
+
       </div>
     </div>
   );
